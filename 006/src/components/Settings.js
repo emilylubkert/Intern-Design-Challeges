d3e04cfe601e42ac209a583f6adac24cd5e2d988
Emily Lubkert <elubkert@gmail.com>
2022-10-31 13:55:41 -0600

    add settings drawer

--- 006/src/components/Settings.js
+++ 006/src/components/Settings.js
@@ -1,53 +1,90 @@
-import { useState } from "react";
-
+import { useState } from 'react';
 
 import {
-    Drawer,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Button,
-  } from '@mui/material';
-  import {
-    CheckBoxOutlineBlankOutlined,
-    DraftsOutlined,
-    HomeOutlined,
-    InboxOutlined,
-    MailOutline,
-    ReceiptOutlined,
-  } from "@mui/icons-material";
-  
+  Drawer,
+  ListItem,
+  ListItemText,
+  Button,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Typography,
+} from '@mui/material';
+
+function Settings() {
+  const [open, setOpen] = useState(false);
+
   const data = [
-    {
-      name: "Home",
-      icon: <HomeOutlined />,
-    },
-    { name: "Measurement Unit", icon: <InboxOutlined /> },
-    { name: "Calendar View", icon: <CheckBoxOutlineBlankOutlined /> },
-    { name: "Saved Locations", icon: <MailOutline /> },
+    { label: 'Measurement Unit', radios: ['\u00b0F', '\u00b0C'] },
+    { label: 'Calendar View', radios: ['M-F', '7-Day', 'Monthly'] },
   ];
-  
-  function Settings() {
-    const [open, setOpen] = useState(false);
-  
-    const getList = () => (
-      <div style={{ width: 250 }} onClick={() => setOpen(false)}>
-        {data.map((item, index) => (
-          <ListItem button key={index}>
-            <ListItemIcon>{item.icon}</ListItemIcon>
-            <ListItemText primary={item.name} />
-          </ListItem>
-        ))}
-      </div>
-    );
-    return (
-      <div>
+
+  const locations = ['Los Angeles', 'San Diego', 'Santa Barbara'];
+
+  return (
+    <div>
+      <div fullWidth className='settings-btn'>
         <Button onClick={() => setOpen(true)}>Settings</Button>
-        <Drawer open={open} anchor={"right"} onClose={() => setOpen(false)}>
-          {getList()}
-        </Drawer>
       </div>
-    );
-  }
-  
-  export default Settings;
+      <Drawer open={open} anchor={'right'} onClose={() => setOpen(false)}>
+      <Button variant="text" color="secondary" sx={{ maxWidth: '20%' }} onClick={() => setOpen(false)}>x</Button>
+        <ListItem>
+          <FormControl>
+            <FormLabel id='units-group-label'>Measurement Unit</FormLabel>
+            <RadioGroup
+              aria-labelledby='units-group-label'
+              name='units-buttons-group'
+            >
+              <FormControlLabel
+                value='Fahrenheit'
+                control={<Radio />}
+                label={`\u2109`}
+              />
+              <FormControlLabel
+                value='Celcius'
+                control={<Radio />}
+                label={`\u00b0C`}
+              />
+            </RadioGroup>
+          </FormControl>
+        </ListItem>
+        <ListItem>
+          <FormControl>
+            <FormLabel id='view-group-label'>Calendar View</FormLabel>
+            <RadioGroup
+              aria-labelledby='view-group-label'
+              name='view-buttons-group'
+            >
+              <FormControlLabel value='M-F' control={<Radio />} label='M-F' />
+              <FormControlLabel
+                value='7-Day'
+                control={<Radio />}
+                label='7-Day'
+              />
+              <FormControlLabel
+                value='Monthly'
+                control={<Radio />}
+                label='Monthly'
+              />
+            </RadioGroup>
+          </FormControl>
+        </ListItem>
+        <ListItem>
+          <FormControl>
+            <FormLabel id='locations-group-label'>Saved Locations</FormLabel>
+            <RadioGroup
+              aria-labelledby='locations-group-label'
+              name='locations-buttons-group'
+            >
+              {locations.map((item) => <FormControlLabel value={item} control={<Radio />} label={item} />)}
+            </RadioGroup>
+          </FormControl>
+        </ListItem>
+      </Drawer>
+    </div>
+  );
+}
+
+export default Settings;
